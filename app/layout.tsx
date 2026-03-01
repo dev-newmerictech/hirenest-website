b0899a48506269eb152bb6c7b83cd1c5a2367b72
amrut1415 <amrut.sankpal@newmerictech.com>
2026-03-01 09:59:28 +0530

    fixed issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -214,10 +214,10 @@ export default function RootLayout({
           strategy="afterInteractive"
         />
         <Script id="plausible-init" strategy="afterInteractive">
-          {\`
+          {`
           window.plausible=window.plausible||function(){(plausible.q = plausible.q || []).push(arguments)},plausible.init=plausible.init||function(i){plausible.o = i || {}};
           plausible.init()
-          \`}
+          `}
         </Script>
       </head>
       <body className={`${inter.variable} antialiased`}>
